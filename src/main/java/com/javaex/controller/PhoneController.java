2b1f44dcf80a1a0a7184668d75066c12c49a80e6
한상선 <hs94122@naver.com>
2022-01-20 18:19:23 +0900

    2022-01-20 18:19

--- src/main/java/com/javaex/controller/PhoneController.java
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +27,19 @@ public class PhoneController {
 	// 메소드 g/s
 
 	// 메소드 일반
+	@RequestMapping(value = "/list", method = { RequestMethod.GET, RequestMethod.POST })
+	public String list(Model model) {// 디스패쳐가 PersonVo를 생성해서 메모리에 올려줌
+		System.out.println("PhoneController>list");
+
+		// 다오에서 리스트를 가져온다
+		List<PersonVo> personList = phoneDao.getPersonList();
+
+		// 컨트롤러 -->> DS로 데이터를 보낸다
+		model.addAttribute("personList", personList);
+
+		return "list";
+	}
+	
 	@RequestMapping(value = "/writeForm", method = { RequestMethod.GET, RequestMethod.POST })
 	public String writeForm() {
 		System.out.println("PhoneController>writeForm");
@@ -36,19 +48,21 @@ public class PhoneController {
 	}
 
 	@RequestMapping(value = "/write", method = { RequestMethod.GET, RequestMethod.POST })
-	public String write(@RequestParam("name") String name, @RequestParam("hp") String hp,
-			@RequestParam("company") String company) {// 디스패쳐가 PersonVo를 생성해서 메모리에 올려줌
+	public String write(@ModelAttribute PersonVo personVo) {// 디스패쳐가 PersonVo를 생성해서 메모리에 올려줌
 		System.out.println("PhoneController>write");
-
-		System.out.println(name);
-		System.out.println(hp);
-		System.out.println(company);
-
-		PersonVo personVo = new PersonVo(name, hp, company);
 
 		phoneDao.personInsert(personVo);
 
 		return "redirect:/phone/list"; // localhost:8088/phonebook3
+	}
+	
+	@RequestMapping(value = "/delete", method = { RequestMethod.GET, RequestMethod.POST })
+	public String Delete(@RequestParam("personId") int personId) {
+		System.out.println("PhoneController>delete");
+
+		phoneDao.personDelete(personId);
+
+		return "redirect:/phone/list";
 	}
 
 	/*
@@ -60,26 +74,14 @@ public class PhoneController {
 	 * 
 	 * System.out.println(personVo); System.out.println(company); return ""; }
 	 */
-	@RequestMapping(value = "/list", method = { RequestMethod.GET, RequestMethod.POST })
-	public String list(Model model) {// 디스패쳐가 PersonVo를 생성해서 메모리에 올려줌
-		System.out.println("PhoneController>list");
 
-		// 다오에서 리스트를 가져온다
-		List<PersonVo> personList = phoneDao.getPersonList();
-		System.out.println(personList.toString());
-
-		// 컨트롤러 -->> DS로 데이터를 보낸다
-		model.addAttribute("personList", personList);
-
-		return "list";
-	}
 
 	@RequestMapping(value = "/updateForm", method = { RequestMethod.GET, RequestMethod.POST })
 	public String UpdateForm(@RequestParam("personId") int personId, Model model) {
 		System.out.println("PhoneController>updateForm");
 
 		PersonVo personVo = phoneDao.getPerson(personId);
-
+		
 		model.addAttribute("personVo", personVo);
 
 		return "updateForm";
@@ -88,39 +90,29 @@ public class PhoneController {
 	@RequestMapping(value = "/update", method = { RequestMethod.GET, RequestMethod.POST })
 	public String Update(@ModelAttribute PersonVo personVo) {
 		System.out.println("PhoneController>update");
-
 		phoneDao.personUpdate(personVo);
 
 		return "redirect:/phone/list";
 	}
 
-	@RequestMapping(value = "/delete", method = { RequestMethod.GET, RequestMethod.POST })
-	public String Delete(@RequestParam("personId") int personId) {
-		System.out.println("PhoneController>delete");
-
-		phoneDao.personDelete(personId);
-
-		return "redirect:/phone/list";
-	}
-	
-	//RequestParam 에 값이 없으면 디폴트로 정한 값을 부여
-	@RequestMapping(value = "/test", method = { RequestMethod.GET, RequestMethod.POST })
-	public String test(@RequestParam(value = "name") String name,
-					   @RequestParam(value = "age", required = false, defaultValue = "-1") int age) {
-		System.out.println("PhoneController>test");
-
-		System.out.println(name);
-		System.out.println(age);
-
-		return "writeForm";
-	}
-	//PathVariable : Mapping 주소를 변수화
-	@RequestMapping(value = "/view/{no}", method = { RequestMethod.GET, RequestMethod.POST })
-	public String view(@PathVariable("no") int no) {
-		System.out.println("PhoneController>view");
-
-		System.out.println(no + "번글 가져오기");
-		
-		return "writeForm";
-	}
+//	//RequestParam 에 값이 없으면 디폴트로 정한 값을 부여
+//	@RequestMapping(value = "/test", method = { RequestMethod.GET, RequestMethod.POST })
+//	public String test(@RequestParam(value = "name") String name,
+//					   @RequestParam(value = "age", required = false, defaultValue = "-1") int age) {
+//		System.out.println("PhoneController>test");
+//
+//		System.out.println(name);
+//		System.out.println(age);
+//
+//		return "writeForm";
+//	}
+//	//PathVariable : Mapping 주소를 변수화
+//	@RequestMapping(value = "/view/{no}", method = { RequestMethod.GET, RequestMethod.POST })
+//	public String view(@PathVariable("no") int no) {
+//		System.out.println("PhoneController>view");
+//
+//		System.out.println(no + "번글 가져오기");
+//		
+//		return "writeForm";
+//	}
 }
